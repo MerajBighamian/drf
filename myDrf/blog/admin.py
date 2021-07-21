82781187ff15b38f5cc0d1822a448f2ddf5df137
meraj bighamian <=>
2021-07-21 23:34:15 +0430

    add view ArticleList and created template for index page

--- myDrf/blog/admin.py
+++ myDrf/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets=((None,
+        {'fields':('title','content','description','slug')}
+    ),
+    ('Advance option',
+        {
+            'fields': ('author','status','published_at'),
+            'classes': ('collapse',)
+        }
+    ))
