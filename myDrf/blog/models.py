76d74cd08b6c92b54da11770b8895fb20d0c253c
meraj bighamian <=>
2021-07-21 22:54:51 +0430

    create model Article and migrated it

--- myDrf/blog/models.py
+++ myDrf/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    title=models.CharField(max_length=200)
+    description= models.TextField()
+    content=models.TextField()
+    slug=models.SlugField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    published_at=models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
